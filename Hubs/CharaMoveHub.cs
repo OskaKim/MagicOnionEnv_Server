ceb118323eb70b7cc21af2009704bc9ee7c3d1ad
Dohyeong Kim <o2kim0225@naver.com>
2024-02-19 22:00:16 +0900

    initialize

---
+++ Hubs/CharaMoveHub.cs
@@ -0,0 +1,47 @@
+﻿using MagicOnion.Server.Hubs;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameServer.Hubs
+{
+    public class CharaMoveHub : StreamingHubBase<ICharaMoveHub, ICharaMoveHubReceiver>, ICharaMoveHub
+    {
+        private IGroup room;
+        private string userName;
+        IInMemoryStorage<string> storage;
+
+        /// <summary>
+        /// 入室通知
+        /// </summary>
+        /// <param name="roomName">ルーム名</param>
+        /// <param name="userName">ユーザー名</param>
+        /// <returns></returns>
+        public async Task<string[]> JoinAsync(string roomName, string userName)
+        {
+            (room, storage) = await Group.AddAsync(roomName, userName);
+            Broadcast(room).OnJoin(userName);
+            this.userName = userName;
+            return storage.AllValues.ToArray();
+        }
+
+        /// <summary>
+        /// 退室通知
+        /// </summary>
+        /// <returns></returns>
+        public async Task LeaveAsync()
+        {
+            await room.RemoveAsync(Context);
+            Broadcast(room).OnLeave(userName);
+        }
+
+        /// <summary>
+        /// 切断通知
+        /// </summary>
+        /// <returns></returns>
+        protected override ValueTask OnDisconnected()
+        {
+            BroadcastExceptSelf(room).OnLeave(userName);
+            return CompletedTask;
+        }
+    }
+}
---
+++ Services/MyFirstService.cs
@@ -0,0 +1,21 @@
+﻿using MagicOnion;
+using MagicOnion.Server;
+using MyApp.Shared;
+using System;
+
+namespace GameServer.Services
+{
+    public class MyFirstService : ServiceBase<IMyFirstService>, IMyFirstService
+    {
+        public async UnaryResult<SumAsyncResult> SumAsync(SumAsyncParam param)
+        {
+            Console.WriteLine($"Received:{param.X}, {param.Y}");
+
+            var result = new SumAsyncResult() 
+            {
+                Value = param.X + param.Y
+            };
+            return result;
+        }
+    }
+}
